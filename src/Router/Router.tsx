bfc9576a233c1ecbd2961b558968b79f1e86ea62
Fernando Campos <fernando.developments@gmail.com>
2021-11-08 21:09:42 -0300

    acomodo basename, agrego 404 generico

--- src/Router/Router.tsx
+++ src/Router/Router.tsx
@@ -4,15 +4,17 @@ import Home from "../Pages/Home/Home";
 import AddCrypto from "../Pages/AddCrypto/AddCrypto";
 import SeeCrypto from "../Pages/SeeCrypto/SeeCrypto";
 import Edit from "../Pages/Edit/Edit";
+import Page404 from "../Pages/404/Page404";
 
 export default function Router() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/Crypto-Journal">
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/add" component={AddCrypto} />
           <Route path="/see/:symbol" component={SeeCrypto} />
           <Route path="/edit/:id" component={Edit} />
+          <Route path="*" component={Page404} />
         </Switch>
     </BrowserRouter>
   );
