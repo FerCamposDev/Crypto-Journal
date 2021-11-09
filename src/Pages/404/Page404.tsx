56f2ea2b992627c75030378028905cf787b4e106
Fernando Campos <fernando.developments@gmail.com>
2021-11-09 01:18:08 -0300

    armo 404, cambio titulo y descripcion

--- src/Pages/404/Page404.tsx
+++ src/Pages/404/Page404.tsx
@@ -1,9 +1,26 @@
+import Page from "../../components/Page/Page"
+import image404 from '../../assets/images/404.jpeg';
+import { Button, Grid } from "@mui/material";
+import { useHistory } from "react-router";
 
 const Page404 = () => {
+  const history = useHistory();
+
   return (
-    <div>
-      Page not found
-    </div>
+    <Page title='Page not found'>
+      <Grid container justifyContent='center' >
+        <img 
+          src={image404} 
+          alt='404'
+        />
+      </Grid>
+      <Grid container justifyContent='center'>
+        <Button onClick={() => history.push('/')}>Go to home</Button>
+      </Grid>
+      <Grid container justifyContent='center'>
+        <Button onClick={() => history.push('/add')}>Add crypto</Button>
+      </Grid>
+    </Page>
   )
 }
 
